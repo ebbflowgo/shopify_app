51159c4de166919b1d09cc18fd8f86785d2ff69f
Christopher Dolinski <christopher.dolinski@gmail.com>
2016-10-28 21:55:56 -0400

    Update sessions_concern.rb

--- lib/shopify_app/sessions_concern.rb
+++ lib/shopify_app/sessions_concern.rb
@@ -39,6 +39,7 @@ module ShopifyApp
     protected
 
     def authenticate
+      byebug
       if shop_name = sanitize_shop_param(params)
         fullpage_redirect_to "#{main_app.root_path}auth/shopify?shop=#{shop_name}"
       else
